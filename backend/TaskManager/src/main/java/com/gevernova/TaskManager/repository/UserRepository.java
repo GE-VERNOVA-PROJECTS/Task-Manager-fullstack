53f38c12d8abbfc59175d6a004c622ca3fed0c0f
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-22 09:07:14 +0530

    [saptadipa77]: Modified user repo

--- backend/TaskManager/src/main/java/com/gevernova/TaskManager/repository/UserRepository.java
+++ backend/TaskManager/src/main/java/com/gevernova/TaskManager/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.gevernova.TaskManager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.gevernova.TaskManager.entity.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
