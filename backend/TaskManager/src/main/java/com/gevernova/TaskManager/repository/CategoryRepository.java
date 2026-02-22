846571699634fadc39d9a8fa6a52e3ef87cc97f4
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-22 09:08:20 +0530

    [saptadipa77]: Modified category repo

--- backend/TaskManager/src/main/java/com/gevernova/TaskManager/repository/CategoryRepository.java
+++ backend/TaskManager/src/main/java/com/gevernova/TaskManager/repository/CategoryRepository.java
@@ -2,4 +2,6 @@ package com.gevernova.TaskManager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.gevernova.TaskManager.entity.Category;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {}
