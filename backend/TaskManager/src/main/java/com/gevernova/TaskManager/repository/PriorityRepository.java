82ffd1754295162c19bcd5b4f4141c63f933475f
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-22 09:04:05 +0530

    [saptadipa77]: Added priority repo

---
+++ backend/TaskManager/src/main/java/com/gevernova/TaskManager/repository/PriorityRepository.java
@@ -0,0 +1,5 @@
+package com.gevernova.TaskManager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PriorityRepository extends JpaRepository<Priority, Long> {}
