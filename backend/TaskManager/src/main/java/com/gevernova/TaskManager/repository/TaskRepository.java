b7f4b455e8cb78e586136b5a6625b4638ad85556
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-22 09:05:06 +0530

    [saptadipa77]: Added task repo

---
+++ backend/TaskManager/src/main/java/com/gevernova/TaskManager/repository/TaskRepository.java
@@ -0,0 +1,5 @@
+package com.gevernova.TaskManager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {}
