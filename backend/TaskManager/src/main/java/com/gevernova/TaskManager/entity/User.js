3a3261cd8a8230e87ec52cd086c1fdc36d019748
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-22 08:52:21 +0530

    [saptadipa77]: Added user.java

---
+++ backend/TaskManager/src/main/java/com/gevernova/TaskManager/entity/User.js
@@ -0,0 +1,11 @@
+@Entity
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
