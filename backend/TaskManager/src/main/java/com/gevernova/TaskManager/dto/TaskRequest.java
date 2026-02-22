a78e502f6e8711b62074bb61810d06977334905c
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-22 09:09:54 +0530

    [saptadipa77]: Added task request dto

---
+++ backend/TaskManager/src/main/java/com/gevernova/TaskManager/dto/TaskRequest.java
@@ -0,0 +1,13 @@
+package com.gevernova.TaskManager.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class TaskRequest {
+    private String title;
+    private String description;
+    private Long userId;
+    private Long categoryId;
+    private Long priorityId;
+}
